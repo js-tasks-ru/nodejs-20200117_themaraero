d04449d3988707ddc2f2792fd956e3c5dfbcf44b
Evgeny Kudryavtsev <themaraero@gmail.com>
2020-02-06 00:59:46 +0300

    add solution for 5-module 1-task

--- 5-module/1-task/app.js
+++ 5-module/1-task/app.js
@@ -7,10 +7,30 @@ app.use(require('koa-bodyparser')());
 const Router = require('koa-router');
 const router = new Router();
 
+let subscribers = [];
+
 router.get('/subscribe', async (ctx, next) => {
+  const promise = new Promise((resolve) => {
+    subscribers.push(resolve);
+  });
+
+  const message = await promise;
+
+  ctx.body = message;
 });
 
 router.post('/publish', async (ctx, next) => {
+  const message = ctx.request.body.message;
+
+  if (message) {
+    subscribers.forEach((resolve) => {
+      resolve(message);
+    });
+
+    subscribers = [];
+
+    ctx.status = 200;
+  }
 });
 
 app.use(router.routes());
--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -1,5 +1,5 @@
 const app = require('./app');
 
-app.listen(3000, () => {
+app.listen(8000, () => {
   console.log('App is running on http://localhost:3000');
 });
