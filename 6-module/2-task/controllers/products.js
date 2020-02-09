b0789a294c287aa8fbc55c5455e687af17220217
Evgeny Kudryavtsev <themaraero@gmail.com>
2020-02-09 17:09:10 +0300

    add solution for 6-module 2-task

--- 6-module/2-task/controllers/products.js
+++ 6-module/2-task/controllers/products.js
@@ -1,12 +1,36 @@
+const ProductModel = require('../models/Product');
+const productMapper = require('../mappers/productMapper');
+
 module.exports.productsBySubcategory = async function productsBySubcategory(ctx, next) {
-  ctx.body = {};
+  const subcategoryId = ctx.request.query.subcategory;
+
+  if (!subcategoryId) {
+    return next();
+  }
+
+  const products = await ProductModel.find({subcategory: subcategoryId});
+
+  ctx.body = {
+    products: products.map(productMapper),
+  };
 };
 
 module.exports.productList = async function productList(ctx, next) {
-  ctx.body = {};
+  const products = await ProductModel.find();
+
+  ctx.body = {products: products.map(productMapper)};
 };
 
 module.exports.productById = async function productById(ctx, next) {
-  ctx.body = {};
-};
+  const id = ctx.params.id;
 
+  const product = await ProductModel.findById(id);
+
+  if (product) {
+    ctx.body = {
+      product: productMapper(product),
+    };
+  } else {
+    ctx.throw(404, 'Product Not Found');
+  }
+};
---
+++ 6-module/2-task/mappers/productMapper.js
@@ -0,0 +1,11 @@
+module.exports = function productMapper(product) {
+  return {
+    id: product['_id'],
+    title: product.title,
+    images: product.images,
+    category: product.category,
+    subcategory: product.subcategory,
+    price: product.price,
+    description: product.description,
+  };
+};
