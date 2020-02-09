b90b685f6b0e7d3005dd64c877527d3dc9efae48
Evgeny Kudryavtsev <themaraero@gmail.com>
2020-02-09 18:22:12 +0300

    add solution for 6-module 3-task

--- 6-module/3-task/controllers/products.js
+++ 6-module/3-task/controllers/products.js
@@ -1,3 +1,10 @@
+const ProductModel = require('../models/Product');
+const productMapper = require('../mappers/productMapper');
+
 module.exports.productsByQuery = async function productsByQuery(ctx, next) {
-  ctx.body = {products: []};
+  const query = ctx.query.query;
+
+  const products = await ProductModel.find({$text: {$search: query}});
+
+  ctx.body = {products: products.map(productMapper)};
 };
