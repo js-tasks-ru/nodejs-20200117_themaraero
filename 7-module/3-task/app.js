d752ff6593792b417f24b9491c25289e41757a84
Evgeny Kudryavtsev <themaraero@gmail.com>
2020-02-16 15:13:52 +0300

    add solution for 7-module 3-task

--- 7-module/3-task/app.js
+++ 7-module/3-task/app.js
@@ -32,6 +32,8 @@ app.use((ctx, next) => {
   ctx.login = async function(user) {
     const token = uuid();
 
+    await Session.create({token, lastVisit: Date.now(), user});
+
     return token;
   };
 
@@ -44,6 +46,15 @@ router.use(async (ctx, next) => {
   const header = ctx.request.get('Authorization');
   if (!header) return next();
 
+  const token = header.split(' ')[1];
+
+  if (!token) return next();
+
+  const session = await Session.findOne({token}).populate('user');
+  if (!session) ctx.throw(401, 'Неверный аутентификационный токен');
+
+  await Session.updateOne({token}, {$set: {lastVisit: Date.now()}});
+  ctx.user = session.user;
   return next();
 });
 
@@ -56,7 +67,7 @@ router.post('/login', login);
 router.get('/oauth/:provider', oauth);
 router.post('/oauth_callback', handleMongooseValidationError, oauthCallback);
 
-router.get('/me', me);
+router.get('/me', mustBeAuthenticated, me);
 
 app.use(router.routes());
 
